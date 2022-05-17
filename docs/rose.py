b4f419ef8d81e514f97672e3fe888f990b0e1716
Joaquin Gonzalez <joagonzalez@gmail.com>
2022-05-17 14:59:27 -0300

    polar rose and diagrams

---
+++ docs/rose.py
@@ -0,0 +1,9 @@
+# https://plotly.com/python/wind-rose-charts/
+
+import plotly.express as px
+df = px.data.wind()
+print(df)
+fig = px.bar_polar(df, r="frequency", theta="direction",
+                   color="strength", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+fig.show()
--- src/application.py
+++ src/application.py
@@ -1,14 +1,16 @@
+from operator import index
 import os
 import glob
-
 import numpy as pd
 import pandas as pd
-
 import plotly.graph_objects as go
+import plotly.express as px
 
+
+ANIOS = ['2016', '2017', '2018', '2019', '2020', '2021']
+DELITOS = ['Robo', 'Hurto', 'Lesiones', 'Homicidio']
 PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)) + '/../data', '*.csv')
 FILENAMES = glob.glob(PATH)
-print(PATH)
 
 
 def save_csv(dataframe: pd.DataFrame, filename: str) -> None:
@@ -42,25 +44,27 @@ def clear_datasets(df: pd.DataFrame) -> pd.DataFrame:
 def show_data(df: pd.DataFrame) -> None:
     print('2016 to 2021')
     print(df.iloc[1:10])
-    #save_csv(df, 'legacy_2016_2021.csv')
 
 def maps(df: pd.DataFrame):
 
     df['text'] = df['tipo_delito']
-    limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
+    limits = [(0,2),(3,10),(11,20),(21,50),(50,3000),(3100,5100),(5101,7100),(7101,10100),(20100,22101)]
     colors = ["royalblue","crimson","lightseagreen","orange","lightgrey"]
+    
     cities = []
     scale = 5000
 
     fig = go.Figure()
 
-    for i in range(len(df.index)):
+    for i in range(len(limits)):
+        lim = limits[i]
+        df_sub = df[lim[0]:lim[1]]
         fig.add_trace(go.Scattergeo(
             locations=['Argentina'],
             locationmode = 'country names',
-            lon = df['long'],
-            lat = df['lat'],
-            text = df['text'],
+            lon = df_sub['long'],
+            lat = df_sub['lat'],
+            text = df_sub['text'],
             marker = dict(
                 size = 5,
                 color = colors[i%5],
@@ -81,10 +85,34 @@ def maps(df: pd.DataFrame):
 
     fig.show()
 
+
+def rose_df(df: pd.DataFrame) -> pd.DataFrame:
+    df_plot = pd.DataFrame()
+    for anio in ANIOS:
+        for delito in DELITOS:
+            df_filtrado = df[(df['anio'] == int(anio)) & df['tipo_delito'].str.contains(delito, na=False)]
+            print(f'filtramos delito {delito} por año {anio}')
+            #print(df_filtrado)
+            tmp = pd.DataFrame([{'anio': anio, 'tipo_delito': delito, 'cantidad': len(df_filtrado.index)}])
+            df_plot = pd.concat([df_plot.reset_index(drop=True), tmp])
+    # print(df_plot)
+
+    return df_plot
+
+def rose_plot(df:pd.DataFrame) -> pd.DataFrame:
+    fig = px.bar_polar(df, r="cantidad", theta="anio",
+                   title='Evolución de patrones delictivos en CABA [2016-2021]',
+                   color="tipo_delito", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Agsunset)
+    fig.show()
+
 if __name__ == '__main__':
     df = load_datasets(FILENAMES)
+    # save_csv(df, 'legacy_2016_2021.csv')
     print(df.dtypes)
     print(f'Total rows: {len(df)}')
     print(df.groupby(by=['dia']).size().reset_index(name='delitos'))
     show_data(df)
-    maps(df)
+    df_rose = rose_df(df)
+    rose_plot(df_rose)
+    
